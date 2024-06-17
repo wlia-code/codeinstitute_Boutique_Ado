5dc21f6aefc6b8d3d01229c83f5a8280b717b1ef
Wasim Alrawas <wassimrawwas@gmail.com>
2024-06-17 20:28:03 +0000

    added home aåå and templates

--- boutique_ado_ci/settings.py
+++ boutique_ado_ci/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 AUTHENTICATION_BACKENDS = [
@@ -74,7 +75,10 @@ ROOT_URLCONF = 'boutique_ado_ci.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
